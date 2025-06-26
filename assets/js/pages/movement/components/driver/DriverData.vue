9247e261230174a4099cb8a05f9465acb2715578
Antonio J Jiménez González <antoniojjg@gmail.com>
2025-06-26 19:12:46 +0200

    include iso in country select

--- assets/js/pages/movement/components/driver/DriverData.vue
+++ assets/js/pages/movement/components/driver/DriverData.vue
@@ -477,14 +477,23 @@ export default {
     computed: {
         requireProvince() {
             if (!this.driver.country) return false;
-            const name = this.driver.country.name ? this.driver.country.name.toLowerCase() : '';
-            return [
-                'espa\u00f1a',
-                'spain',
-                'canarias',
-                'islas canarias',
-                'canary islands'
-            ].includes(name);
+
+            const isoRaw =
+                this.driver.country.iso ||
+                this.driver.country.countryCode ||
+                this.driver.country.countryISO ||
+                this.driver.country.COUNTRYISO ||
+                this.driver.country.code ||
+                this.driver.country.ISO ||
+                '';
+            const iso = typeof isoRaw === 'string' ? isoRaw.toUpperCase() : '';
+
+            const id = Number(this.driver.country.id);
+
+            const isos = ['ES', 'IC']; // Spain and Canary Islands ISO codes
+            const isIds = [1]; // Spain id
+
+            return !(isos.includes(iso) || isIds.includes(id));
         },
     },
     mounted() {
@@ -737,11 +746,14 @@ export default {
             if (this.driver.provider === null) this.driver.provider = {};
             this.driver.provider.id = this.providerId;
         },
-        'driver.country': function () {
-            if (!this.requireProvince) {
-                this.driver.state = null;
-                this.driver.postalCode = null;
-            }
+        'driver.country': {
+            handler() {
+                if (!this.requireProvince) {
+                    this.driver.state = null;
+                    this.driver.postalCode = null;
+                }
+            },
+            deep: true,
         },
     },
 };
